327d2fada686dedf324b54ec3da866cecf024b4b
Luke Dennis <luke@lukifer.net>
2020-09-27 16:03:33 -0600

    mqtt subscribe fix

--- mqttlisten.ts
+++ mqttlisten.ts
@@ -8,14 +8,14 @@ const { MQTT_IP, URL_MOPIDY } = config;
 
 const mqttClient = connect(`mqtt://${MQTT_IP}`);
 
+mqttClient.on("connect", () => {
+	mqttClient.subscribe(["voice2json"], () => {});
+});
+
 const mopidy = new Mopidy({ webSocketUrl: URL_MOPIDY });
 
 mopidy.on("state:online", async () => {
 	await mopidy.tracklist.clear();
-
-	mqttClient.on("connect", () => {
-		mqttClient.subscribe(["voice2json"], () => {});
-	});
 
 	mqttClient.on("message", (_topic, message) => {
 		doIntent(mopidy, JSON.parse(message.toString()));
